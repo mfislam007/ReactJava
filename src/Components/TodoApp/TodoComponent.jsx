3ee07aaf9887aadd1e1412b0422837b9a8702867
Md Farukul Islam <mfislam007@yahoo.com>
2020-05-05 11:12:55 +0300

    create new data

--- src/Components/TodoApp/TodoComponent.jsx
+++ src/Components/TodoApp/TodoComponent.jsx
@@ -1,8 +1,109 @@
 import React, { Component } from 'react';
+import moment from 'moment';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import TodoDataService from '../../api/todo/TodoDataService.js';
+import AuthenticationService from './AuthenticationService.js';
 
 class TodoComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      id: this.props.match.params.id,
+      description: '',
+      targetDate: moment(new Date()).format('YYYY-MM-DD'),
+    };
+
+    this.onSubmit = this.onSubmit.bind(this);
+    this.validate = this.validate.bind(this);
+  }
+
+  componentDidMount() {
+    let username = AuthenticationService.getLoggedInUserName();
+
+    TodoDataService.retrieveTodo(username, this.state.id).then((response) =>
+      this.setState({
+        description: response.data.description,
+        targetDate: moment(response.data.targetDate).format('YYYY-MM-DD'),
+      })
+    );
+  }
+
+  onSubmit(values) {
+    console.log(values);
+  }
+
+  validate(values) {
+    let errors = {};
+    if (!values.description) {
+      errors.description = 'Enter a Description';
+    } else if (values.description.length < 5) {
+      errors.description = 'Enter atleast 5 characters in Description';
+    }
+
+    if (!moment(values.targetDate).isValid()) {
+      errors.targetDate = 'Enter a valid TargetDate';
+    }
+    return errors;
+  }
+
   render() {
-    return <div>Todo Component for id - {this.props.match.params.id}</div>;
+    let { description, targetDate } = this.state;
+    //let targetDate = this.state.targetDate;
+
+    return (
+      <div>
+        <h1>Todo</h1>
+        <div className='container'>
+          <Formik
+            initialValues={{
+              description,
+              targetDate,
+            }}
+            onSubmit={this.onSubmit}
+            validateOnChange={false}
+            validateOnBlur={false}
+            validate={this.validate}
+            enableReinitialize={true}
+          >
+            {(props) => (
+              <Form>
+                <ErrorMessage
+                  name='description'
+                  component='div'
+                  className='alert alert-warning'
+                />
+                <ErrorMessage
+                  name='targetDate'
+                  component='div'
+                  className='alert alert-warning'
+                />
+                <fieldset className='form-group'>
+                  <label> Description</label>
+                  <Field
+                    className='form-control'
+                    type='text'
+                    name='description'
+                  ></Field>
+                </fieldset>
+                <fieldset className='form-group'>
+                  <label> Target date</label>
+                  <Field
+                    className='form-control'
+                    type='date'
+                    name='targetDate'
+                  ></Field>
+                </fieldset>
+                <button className='btn btn-success' type='submit'>
+                  Save
+                </button>
+              </Form>
+            )}
+          </Formik>
+        </div>
+        Todo Component for id - {this.props.match.params.id}
+      </div>
+    );
   }
 }
 
--- src/api/todo/TodoDataService.js
+++ src/api/todo/TodoDataService.js
@@ -6,6 +6,11 @@ class TodoDataService {
     return axios.get(`http://localhost:8080/users/${name}/todos`);
   }
 
+  retrieveTodo(name, id) {
+    //console.log('executed service')
+    return axios.get(`http://localhost:8080/users/${name}/todos/${id}`);
+  }
+
   deleteTodo(name, id) {
     //console.log('executed service')
     return axios.delete(`http://localhost:8080/users/${name}/todos/${id}`);
